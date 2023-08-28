9ed9a09792a8ccb0578495622497825039aabea1
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-08-28 22:54:33 +0530

    Update database.py

--- bot/database/database.py
+++ bot/database/database.py
@@ -525,5 +525,3 @@ class Database(metaclass=Singleton):
         A Funtion to count total filters of a group
         """
         return await self.fcol.count_documents({"group_id": group_id})
-
-db = Database(DATABASE_URI, DATABASE_NAME)
