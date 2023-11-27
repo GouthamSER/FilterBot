ce8d1db22f86573cbf25bb074588e43e1ae3eed3
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-11-27 18:50:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -41,26 +41,22 @@ Request Yourself...!! ✔
 © Kuttu Bot
 """
 
-    CONFIGS_TXT = """<code> /add </code>
+    CONNECTION_TXT = """<code> /add </code>
 channel id or username
 Connect a Channel TO add Movies and Series
 eg-<code>channel id or username</code>
-<code>/settings</code>
-This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-<code>Channel</code>
-Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls           
-<code>Filter Types</code>
-Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-<code>Configure</code>
-Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-<code>Status</code>
-Button Will Shows The Stats Of Your Channel
-<code>/del chat_id</code>
+"""
+
+    DELETE_TXT = """<code>/del chat_id</code>
 To disconnect A Group With A Channel
 or
 <code>/del @Username</code>
 <code>/delall</code>
 This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB"""
 
-    RESULT_TXT = """Hᴇʀᴇ Is Wʜᴀᴛ I Fᴏᴜɴᴅ Fᴏʀ Yᴏᴜʀ Sᴇaʀᴄʜ
+    SETTING_TXT = """<code>/settings</code>
+This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+<code>Channel</code>
+Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
 """
+    RESULT_TXT = """Hᴇʀᴇ Is Wʜᴀᴛ I Fᴏᴜɴᴅ Fᴏʀ Yᴏᴜʀ Sᴇaʀᴄʜ"""
